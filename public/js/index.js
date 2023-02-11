04ae31d2535c361cbfc33ccbc51593add7c880ad
Polar Bear <xadjimurodaliyev@gmail.com>
2023-02-12 00:33:28 +0500

    make benefit cards

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,11 @@
 let scrolling = document.body
 const main = document.querySelector(".main")
 const footer = document.querySelector(".footer")
-const calcullateHeight = main.offsetHeight - footer.offsetHeight - 663
+const calcullateHeight = main.offsetHeight - 663
 console.log(calcullateHeight)
 
 window.addEventListener("scroll", function(){
-	if(window.scrollY >= 577.5999755859375 && window.scrollY < calcullateHeight){
+	if(window.scrollY >= 600 && window.scrollY < calcullateHeight){
 		scrolling.classList.add("__white")
 		console.log(window.scrollY)
 	}else{
